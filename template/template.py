9d523080d6bf3970cd8272f4ab17f771692729e7
nullnull <nullnullprogramming@gmail.com>
2021-03-20 23:04:57 +0900

    update

--- template/template.py
+++ template/template.py
@@ -5,9 +5,10 @@ file_name='./chall'
 context.binary = file_name
 context.log_level = 'debug'
 
-HOST = 'target'
-PORT = 1337
-conn = None
+    def interactive(self):
+        for log in logs:
+            for key,value in log.items():
+                log.info("{} : {}".format(key,value))
 
 if len(sys.argv) > 1 and sys.argv[1] == 'r':
     conn = remote(HOST, PORT)
