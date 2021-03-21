2368dd573eb2d66fa71fd75b7abed98f2c0a366f
nullnull <nullnullprogramming@gmail.com>
2021-03-21 20:00:19 +0900

    update

--- template/template.py
+++ template/template.py
@@ -20,9 +20,9 @@ class Pwn:
 						libc=ELF('./'+file)
 						conn=process(elf_name,env={'LD_PRELOAD':file})
 
-				if len(sys.argv)>1 and sys.argv[1]=='r':
-					context.log_level='INFO'
-					conn=remote(host_port.split(' ')[0],host_port.split(' ')[1])
+		if len(sys.argv)>1 and sys.argv[1]=='r':
+			context.log_level='INFO'
+			conn=remote(host_port.split(' ')[0],host_port.split(' ')[1])
 
 		if libc==None:
 			self.solver(conn,logs,elf)
@@ -42,56 +42,3 @@ class Pwn:
 
 if __name__=='__main__':
 	pwn=Pwn('HOST_PORT')
-
-{
-	"pwn template": {
-	"prefix": "temp",
-	  "body": [
-		"from pwn import *",
-		"import os",
-		"import magic",
-		"",
-		"class Pwn:",
-		"",
-			"\tdef __init__(self,host_port=None,conn=None,elf=None,libc=None,logs={}):",
-				"\t\telf_name=None",
-				"\t\tcontext.log_level='DEBUG'",
-		"",
-				"\t\tfor cnt in range(2):",
-					"\t\t\tfor file in os.listdir(os.getcwd()):",
-						"\t\t\t\tif 'ELF' in magic.from_file(file):",
-							"\t\t\t\t\tif cnt==0 and not 'libc' in file and file!='core':",
-								"\t\t\t\t\t\telf_name=file",
-								"\t\t\t\t\t\telf=ELF('./'+file)",
-								"\t\t\t\t\t\tcontext.binary=file",
-								"\t\t\t\t\t\tconn=process(file)",
-							"\t\t\t\t\telif cnt==1 and 'libc' in file:",
-								"\t\t\t\t\t\tlibc=ELF('./'+file)",
-								"\t\t\t\t\t\tconn=process(elf_name,env={'LD_PRELOAD':file})",
-				"",		
-				"\t\t\t\tif len(sys.argv)>1 and sys.argv[1]=='r':",
-					"\t\t\t\t\tcontext.log_level='INFO'",
-					"\t\t\t\t\tconn=remote(host_port.split(' ')[0],host_port.split(' ')[1])",
-				"",
-				"\t\tif libc==None:",
-					"\t\t\tself.solver(conn,logs,elf)",
-				"\t\telse:",
-					"\t\t\tself.solver(conn,logs,elf,libc)",
-				"",
-				"\t\tself.interactive(conn,logs)",
-				"",
-			"\tdef interactive(self,conn,logs):",
-				"\t\tfor key,value in logs.items():",
-					"\t\t\tlog.info('{}'.format(key).ljust(15,' ')+':{}'.format(hex(value)))",
-			"",
-				"\t\tconn.interactive()",
-			"",
-			"\tdef solver(self,conn,logs,elf,libc=None):",
-			"\t\treturn",
-			"",
-		"if __name__=='__main__':",
-			"\tpwn=Pwn('HOST_PORT')",
-	  ],
-     "description": "template for pwn"
-    }
-}
